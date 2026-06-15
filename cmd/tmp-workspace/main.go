ae34ee84b35f972fd9cc3932b5df345241c128d9
IridiumNan <2930416610@qq.com>
2026-06-15 14:52:22 +0800

    fix the config load bug

--- cmd/tmp-workspace/main.go
+++ cmd/tmp-workspace/main.go
@@ -1,14 +1,4 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/IridiumNan/tmp-workspace/internal/config"
-)
-
 func main() {
-	err := config.LoadConfig()
-	if err != nil {
-		fmt.Println(err)
-	}
 }
